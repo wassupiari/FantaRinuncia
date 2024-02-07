63378dd5259ce61a810dafc5d87699c0fb6a32c6
jarek <48630736+wassupiari@users.noreply.github.com>
2024-02-07 17:18:58 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
+import json
+import os
+
+app = Flask(__name__)
+app.secret_key = 'il_tuo_segreto'
+
+# Percorso del file JSON per le squadre
+squadra_json_file = 'squadra.json'
+utenti_json_file = 'utenti.json'
+db_json_file = 'people.json'
+
+@app.route('/auth/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Carica gli utenti esistenti
+        utenti = carica_utenti()
+
+        # Verifica se l'utente esiste e le credenziali sono corrette
+        if username in utenti and utenti[username] == password:
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return 'Credenziali non valide. <a href="/auth/login">Riprova</a>'
+    return render_template('auth/login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+# Carica dati delle persone
+with open(db_json_file, 'r') as file:
+    people = json.load(file)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return render_template('index.html', data=people)
+    return 'Non sei loggato <a href="/auth/login">Login</a>'
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    return jsonify(people)
+
+@app.route('/crea-squadra', methods=['POST'])
+def crea_squadra():
+    if 'username' in session:
+        username = session['username']
+        data = request.json
+        selected_names = data.get('squadra', [])
+
+        # Carica squadre esistenti se il file esiste, altrimenti inizializza un nuovo dizionario vuoto
+        if os.path.exists(squadra_json_file):
+            try:
+                with open(squadra_json_file, 'r') as file:
+                    squadre = json.load(file)
+            except json.decoder.JSONDecodeError:
+                squadre = {}
+        else:
+            squadre = {}
+
+        # Verifica se l'utente ha già una squadra e restituisci un messaggio di errore se è così
+        if username in squadre:
+            return 'Hai già creato una squadra. Per creare una nuova squadra, cancella prima quella esistente.'
+
+        # Aggiungi la nuova squadra per l'utente
+        squadre[username] = selected_names
+
+        # Salva tutte le squadre aggiornate sul file JSON
+        with open(squadra_json_file, 'w') as file:
+            json.dump(squadre, file)
+
+        return 'Squadra creata e salvata.'
+    return 'Non sei loggato <a href="/auth/login">Login</a>'
+
+@app.route('/visualizza-squadre')
+def visualizza_squadre():
+    if 'username' in session:
+        if os.path.exists(squadra_json_file):
+            with open(squadra_json_file, 'r') as file:
+                squadre = json.load(file)
+                username = session['username']
+                return render_template('/src/views.html', squadre=squadre, username=username)
+        else:
+            return 'Nessuna squadra trovata'
+    return 'Non sei loggato <a href="/auth/login">Login</a>'
+
+# Funzione per caricare gli utenti dal file JSON
+def carica_utenti():
+    if os.path.exists(utenti_json_file):
+        with open(utenti_json_file, 'r') as file:
+            return json.load(file)
+    else:
+        return {}
+
+# Funzione per salvare gli utenti nel file JSON
+def salva_utenti(utenti):
+    with open(utenti_json_file, 'w') as file:
+        json.dump(utenti, file)
+
+@app.route('/auth/register', methods=['GET', 'POST'])
+def registrazione():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Carica gli utenti esistenti
+        utenti = carica_utenti()
+
+        # Controlla se l'username è già presente
+        if username in utenti:
+            return 'Username già esistente. <a href="/auth/register">Riprova</a>'
+        else:
+            # Aggiunge il nuovo utente al dizionario
+            utenti[username] = password
+            # Salva il dizionario aggiornato nel file JSON
+            salva_utenti(utenti)
+            return 'Registrazione completata. <a href="/auth/login">Effettua il login</a>'
+    return render_template('auth/register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Array per memorizzare le persone selezionate
+    var squadra = [];
+
+    // Funzione per aggiornare la griglia delle persone selezionate
+    function updateSquadraGrid() {
+        var squadraContainer = document.getElementById("squadra-container");
+        squadraContainer.innerHTML = "";
+
+        squadra.forEach(function(person) {
+            var cardElement = document.createElement("div");
+            cardElement.classList.add("card");
+            cardElement.innerHTML = '<div class="card-body"><p class="card-text">' + person + '</p></div>';
+            squadraContainer.appendChild(cardElement);
+        });
+    }
+
+    // Selezionatore per le card e gestione degli eventi
+    var cardsContainer = document.getElementById("cards-container");
+    cardsContainer.addEventListener("change", function(event) {
+        var checkbox = event.target;
+        var card = checkbox.closest(".card");
+        var cardText = card.querySelector(".card-text").textContent;
+
+        if (checkbox.checked) {
+            squadra.push(cardText);
+        } else {
+            var index = squadra.indexOf(cardText);
+            if (index !== -1) {
+                squadra.splice(index, 1);
+            }
+        }
+
+        updateSquadraGrid();
+    });
+
+    // Funzione per gestire l'evento di clic sul pulsante "Salva Squadra"
+    document.getElementById("salva-squadra-button").addEventListener("click", function() {
+        salvaSquadra();
+    });
+
+    // Funzione per salvare la squadra su un file JSON
+    function salvaSquadra() {
+        fetch('/crea-squadra', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ squadra: squadra }),
+        })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+                var alertContainer = document.getElementById("alert-container");
+                alertContainer.innerHTML = '';
+
+                // Crea e aggiungi l'alert Bootstrap al container
+                var alertDiv = document.createElement("div");
+                alertDiv.classList.add("alert", "alert-success", "alert-dismissible", "fade", "show");
+                alertDiv.setAttribute("role", "alert");
+                alertDiv.innerHTML = `
+                    Squadra creata e salvata.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                `;
+                alertContainer.appendChild(alertDiv);
+        })
+        .catch(error => {
+            console.error('Errore:', error);
+        });
+    }
+
+    // Funzione per gestire l'evento di clic sul pulsante di ricerca
+    document.getElementById("search-button").addEventListener("click", function() {
+        searchCards();
+    });
+
+    // Funzione per gestire l'evento di pressione del tasto "Invio" nella casella di ricerca
+    document.getElementById("search-input").addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            searchCards();
+        }
+    });
+
+    // Funzione per filtrare le card in base alla query di ricerca
+    function searchCards() {
+        var searchQuery = document.getElementById("search-input").value.toLowerCase();
+        var cardItems = document.querySelectorAll(".card-item");
+
+        cardItems.forEach(function(cardItem) {
+            var cardText = cardItem.querySelector(".card-text").textContent.toLowerCase();
+            if (cardText.includes(searchQuery)) {
+                cardItem.style.display = "block";
+            } else {
+                cardItem.style.display = "none";
+            }
+        });
+    }
+});
