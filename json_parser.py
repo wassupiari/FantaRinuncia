0fc9c8f9a5f1629cd1591cd1e0f8c366b8d8659b
jarek <48630736+wassupiari@users.noreply.github.com>
2024-02-09 18:01:11 +0100

    some changes

---
+++ json_parser.py
@@ -0,0 +1,17 @@
+import json
+import random
+
+def aggiungi_punti(json_file, nuovo_json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+    for entry in data:
+        entry["points"] = random.randint(5, 20)
+
+    with open(nuovo_json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+# Utilizzo dello script
+json_file = "people.json"  # Imposta il percorso del file JSON da leggere
+nuovo_json_file = "people.json"  # Imposta il percorso del nuovo file JSON
+aggiungi_punti(json_file, nuovo_json_file)
